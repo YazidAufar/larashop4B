4db37eceb2e31e93ec335bb0cef39f0daa62f502
Yazid Aufar <yazid.aufar.ya@gmail.com>
2023-04-10 23:22:37 +0800

    Manage Category

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -3,17 +3,7 @@
 @section('title') Category list @endsection
 
 @section('content')
-@if(session('status'))
   <div class="row">
-    <div class="col-md-12">
-      <div class="alert alert-warning">
-        {{session('status')}}
-      </div>
-    </div>
-  </div>
-@endif
-
-<div class="row">
     <div class="col-md-6">
       <form action="{{route('categories.index')}}">
 
@@ -48,65 +38,82 @@
     </div>
 
   </div>
-<hr class="my-3">
 
-<div class="row">
+  <hr class="my-3">
+
+  @if(session('status'))
+    <div class="row">
+      <div class="col-md-12">
+        <div class="alert alert-warning">
+          {{session('status')}}
+        </div>
+      </div>
+    </div>
+  @endif
+
+  <div class="row">
+    <div class="col-md-12 text-right">
+      <a href="{{route('categories.create')}}" class="btn btn-primary">Create category</a>
+    </div>
+  </div>
+  <br>
+
+  <div class="row">
     <div class="col-md-12">
-        <table class="table table-bordered table-stripped">
+      <table class="table table-bordered table-stripped">
         <thead>
-            <tr>
+          <tr>
             <th><b>Name</b></th>
             <th><b>Slug</b></th>
             <th><b>Image</b></th>
             <th><b>Actions</b></th>
-            </tr>
+          </tr>
         </thead>
 
         <tbody>
-            @foreach ($categories as $category)
+          @foreach ($categories as $category)
             <tr>
-                <td>{{$category->name}}</td>
-                <td>{{$category->slug}}</td>
-                <td>
+              <td>{{$category->name}}</td>
+              <td>{{$category->slug}}</td>
+              <td>
                 @if($category->image)
-                    <img
-                     src="{{asset('storage/' . $category->image)}}"
-                     width="48px"/>
+                  <img src="{{asset('storage/' . $category->image)}}" width="48px"/>
                 @else
-                    No image
+                  No image
                 @endif
-                </td>
-                <td>
-                    <a
-                    href="{{route('categories.edit', [$category->id])}}"
-                    class="btn btn-info btn-sm"> Edit </a>
-                    <a
-                    href="{{route('categories.show', [$category->id])}}"
-                    class="btn btn-primary btn-sm"> Show </a>
+              </td>
+              <td>
+                <a
+                  href="{{route('categories.edit', [$category->id])}}"
+                  class="btn btn-info btn-sm"> Edit </a>
 
-                    <form
-                    class="d-inline"
-                    action="{{route('categories.destroy', [$category->id])}}"
-                    method="POST"
-                    onsubmit="return confirm('Move category to trash?')"
-                    >
+                <a
+                  href="{{route('categories.show', [$category->id])}}"
+                  class="btn btn-primary"> Show </a>
 
-                    @csrf
+                <form
+                  class="d-inline"
+                  action="{{route('categories.destroy', [$category->id])}}"
+                  method="POST"
+                  onsubmit="return confirm('Move category to trash?')"
+                  >
 
-                    <input
-                        type="hidden"
-                        value="DELETE"
-                        name="_method">
+                  @csrf
 
-                    <input
-                        type="submit"
-                        class="btn btn-danger btn-sm"
-                        value="Trash">
+                  <input
+                    type="hidden"
+                    value="DELETE"
+                    name="_method">
 
-                    </form>
-                </td>
+                  <input
+                    type="submit"
+                    class="btn btn-danger btn-sm"
+                    value="Trash">
+
+                </form>
+              </td>
             </tr>
-            @endforeach
+          @endforeach
         </tbody>
         <tfoot>
           <tr>
@@ -115,7 +122,8 @@
             </td>
           </tr>
         </tfoot>
-        </table>
+
+      </table>
     </div>
-</div>
+  </div>
 @endsection
