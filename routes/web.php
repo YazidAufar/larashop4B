dff201a77a687c69eafb8161e8541b21d177a464
Yazid Aufar <yazid.aufar.ya@gmail.com>
2023-04-11 11:01:20 +0800

    Manage Book

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\BookController;
 
 Route::get('/', function(){
     return view('auth.login');
@@ -19,4 +20,6 @@ Route::get('/categories/trash', [CategoryController::class, 'trash'])->name('cat
 Route::get('/categories/{id}/restore', [CategoryController::class, 'restore'])->name('categories.restore');
 Route::delete('/categories/{category}/delete-permanent', [CategoryController::class, 'deletePermanent'])->name('categories.delete-permanent');
 Route::resource('categories', CategoryController::class);
+Route::resource('books', BookController::class);
+Route::get('/ajax/categories/search', [CategoryController::class, 'ajaxSearch']);
 
